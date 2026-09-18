9ed21a20a5ab88f1f3b0f34ee739ae2a0e4e74b0
aes011204 <aes011204@gmail.com>
2026-09-18 22:46:08 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.01 ms, Memory: 4.94 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long solution(int price, int money, int count) {
+    long long answer = 0;
+    
+    
+    for(int i =1; i <= count; i++)
+    {
+answer+=price*i;
+
+    }
+    
+    answer-=money;
+    if (answer<0)
+        answer=0;
+    
+    
+    
+    return answer;
+}
